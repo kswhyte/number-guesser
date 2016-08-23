7859ca833b8f88ddfba6a5dfd1e74cd646d969ca
gness1804 <graham.nessler@gmail.com>
2016-08-22 19:10:23 -0600

    Did much of the project; need to do number generator

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
 function clearBox() {
-  document.getElementById("numberChosen").value = "";
-};
+  document.getElementById("numberInput").value = "";
+  //call disable function here
+}
+
+function buttonsDisabled () {
+  var guessBox = document.getElementById("numberInput").value;
+  if (guessBox === "") {
+    document.getElementById("clear-button").disabled = true;
+    document.getElementById("reset-button").disabled = true;
+  }
+}
+
+function buttonsEnabled () {
+  var guessBox = document.getElementById("numberInput").value;
+  if (guessBox !== "") {
+    document.getElementById("clear-button").disabled = false;
+    document.getElementById("reset-button").disabled = false;
+  }
+}
+
+
 
 function reset () {
-
-};
+  clearBox();
+  document.getElementById("user-feedback").innerHTML = "Are you feeling lucky today?";
+  document.getElementById("number-generator").innerHTML = "N/A";
+  //needs other stuff too
+}
 
 function generate () {
-  var userNumber = document.getElementById("numberChosen").value;
+  var userNumber = document.getElementById("numberInput").value;
   var realInput = parseInt(userNumber);
 
   if (realInput > 100) {
@@ -20,4 +42,12 @@ function generate () {
     document.getElementById('user-feedback').innerHTML = "Oops, you need to choose a number.";
   }
 
-};
+}
+
+function setRange () {
+  var inputRange = document.getElementById("numberInput");
+  var newMin = document.getElementById("minimum").value;
+  var newMax = document.getElementById("maximum").value;
+  inputRange.setAttribute("min", newMin);
+  inputRange.setAttribute("max", newMax);
+}
