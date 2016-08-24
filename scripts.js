d56b7d9ddc00cf69b1a1126096e2eafee112d592
gness1804 <graham.nessler@gmail.com>
2016-08-23 19:18:49 -0600

    Got the max and min assignments worked out

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-var guessInput = document.querySelector("#guess-input");
+var guessInput = document.querySelector("#guess-input"); // number input field
 var clearButton = document.getElementById("clear-button");
 var resetButton = document.getElementById("reset-button");
+var guessButton = document.getElementById("guess-button"); //the button they click to generate the number
+var submitButton = document.getElementById("submit-button"); // the button to change max/min values
+var minButton = document.getElementById("minimum"); // the number they enter as the new min
+var maxButton = document.getElementById("maximum"); // the number they enter as the new min
+var min = 0;
+var max = 100;
 
 // document.onload=function () {
 guessInput.addEventListener("keydown", function () {
@@ -35,42 +41,56 @@ function clearBox() {
   buttonsDisabled();
 }
 
-// ------------------------
+resetButton.addEventListener("click", function () {
+  reset();
+});
 
 function reset () {
   clearBox();
   document.getElementById("user-feedback").innerHTML = "Are you feeling lucky today?";
-  document.getElementById("number-generator").innerHTML = "N/A";
+  document.getElementById("number-generator").innerHTML = "X";
   //needs other stuff too
 }
 
-var example = document.getElementById('guess-button');
+guessButton.addEventListener("click", function () {
+  generate();
+});
+
+function generate() {
+  var numberTheyChose = guessInput.value;
+  var realNumberTheyChose = parseInt(numberTheyChose);
+
+  //random number generator here?
+
+  if (realNumberTheyChose > max) {
+    document.getElementById("user-feedback").innerHTML = "Your number is too high; try again.";
+  }
+  else if (realNumberTheyChose < min) {
+    document.getElementById("user-feedback").innerHTML = "Your number is too low; try again.";
+  }
+  else if (isNaN(realNumberTheyChose)) {
+    document.getElementById('user-feedback').innerHTML = "Oops, you need to choose a number.";
+  }
+} // end of generate()
+
+submitButton.addEventListener("click", function () {
+  reset();
+  setRange();
+});
+
+function setRange () {
+  var theirNewMin = minButton.value;
+  var realTheirNewMin = parseInt(theirNewMin);
+  var theirNewMax = maxButton.value;
+  var realTheirNewMax = parseInt(theirNewMax);
+  min = realTheirNewMin;
+  max = realTheirNewMax;
+}
 
 
-/*example.addEventListener('click', function(){
-  alert('hello');
-});*/
+//submit: onClick="reset();setRange()
 
-// function generate () {
-//   var userNumber = document.getElementById("numberInput").value;
-//   var realInput = parseInt(userNumber);
-//   var currentMin = document.getElementById("numberInput").getAttribute("min");
-//
-//
-//   if (realInput > 100) {
-//     document.getElementById("user-feedback").innerHTML = "Your number is too high; try again.";
-//   }
-//   else if (realInput < 1) {
-//     document.getElementById("user-feedback").innerHTML = "Your number is too low; try again.";
-//   }
-//   else if (isNaN(realInput)) {
-//     document.getElementById('user-feedback').innerHTML = "Oops, you need to choose a number.";
-//   }
-//
-// }
-//
-// var min = 0;
-// var max = 100;
+
 //
 // function setRange () {
 //   var inputRange = document.getElementById("numberInput");
